f3749b6086c8f07020a1d6e8c967943ce2b241ff
michalrudy9 <michallrudy9@gmail.com>
2024-03-04 19:39:47 +0100

    Provided colors, font family and tag style of project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,18 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      violet: "#9E7F66",
+      "light-violet": "#939BF4",
+      "very-dark-blue": "#19202D",
+      midnight: "#121721",
+      "light-grey": "#F4F6F8",
+      gray: "#9DAEC2",
+      "dark-grey": "#6E8098",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        "kumbh-sans": ["Kumbh Sans", "sans-serif"],
       },
     },
   },
