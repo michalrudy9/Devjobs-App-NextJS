1dfe17d0948a624b8bd8f205afd5ae0a58c614c8
michalrudy9 <michallrudy9@gmail.com>
2024-03-05 19:15:49 +0100

    Updated primary button component

--- app/ui/PrimaryButton.tsx
+++ app/ui/PrimaryButton.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const PrimaryButton: React.FC<{ text: string; className: string }> = ({
+const PrimaryButton: React.FC<{ text: string; className?: string }> = ({
   text,
   className,
 }) => {
-  const style: string = `bg-violet py-3 px-6 font-bold text-white rounded-md hover:bg-light-violet${className}`;
+  const style: string = `bg-violet py-3 px-6 font-bold text-white rounded-md hover:bg-light-violet ${className}`;
 
   return <button className={style}>{text}</button>;
 };
