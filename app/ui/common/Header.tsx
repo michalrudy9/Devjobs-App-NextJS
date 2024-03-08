a33c031b11b08633575dd858f0a42ecd980aa568
michalrudy9 <michallrudy9@gmail.com>
2024-03-08 14:05:30 +0100

    Added translate for logo and switch mode

--- app/ui/common/Header.tsx
+++ app/ui/common/Header.tsx
@@ -5,8 +5,8 @@ import SwitchMode from "./SwitchMode";
 
 const Header = () => {
   return (
-    <header className="bg-mobile-pattern-header md:bg-tablet-pattern-header lg:bg-desktop-pattern-header bg-no-repeat bg-cover p-5 lg:p-10">
-      <div className="flex justify-between items-center">
+    <header className="bg-mobile-pattern-header md:bg-tablet-pattern-header lg:bg-desktop-pattern-header bg-no-repeat bg-cover p-5 lg:p-10 2xl:px-[10rem]">
+      <div className="flex justify-between items-center translate-y-5">
         <h1 className="text-white">
           <Link href="/">devjobs</Link>
         </h1>
