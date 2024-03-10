6a640187491eb596fbd97e8b181a7ca03882a6a9
michalrudy9 <michallrudy9@gmail.com>
2024-03-10 11:14:14 +0100

    Provided changing bg via redux

--- app/ui/common/SwitchMode.tsx
+++ app/ui/common/SwitchMode.tsx
@@ -2,13 +2,21 @@ import Image from "next/image";
 
 import iconSun from "@/public/desktop/icon-sun.svg";
 import iconMoon from "@/public/desktop/icon-moon.svg";
+import { useAppDispatch } from "@/store/hooks";
+import { toggle } from "@/store/modeSlice";
 
 const SwitchMode = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <div className="flex items-center gap-x-4">
       <Image src={iconSun} alt="Sun icon" className="w-auto h-full" />
       <label className="inline-flex items-center cursor-pointer">
-        <input type="checkbox" value="" className="sr-only peer" />
+        <input
+          type="checkbox"
+          className="sr-only peer"
+          onClick={() => dispatch(toggle())}
+        />
         <div className="relative w-10 h-5 bg-white rounded-full peer-checked:after:translate-x-5 after:absolute after:top-[4px] after:start-[4px] after:bg-violet after:hover:bg-light-violet after:rounded-full after:h-3 after:w-3 after:transition-all"></div>
       </label>
       <Image src={iconMoon} alt="Moon icon" className="w-auto h-full" />
