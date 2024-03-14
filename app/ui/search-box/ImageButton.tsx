7a8ab35ab54b96954ed02aca6996dd5190cb4393
michalrudy9 <michallrudy9@gmail.com>
2024-03-14 22:44:09 +0100

    Added spinner and showing spinner if form pending

--- app/ui/search-box/ImageButton.tsx
+++ app/ui/search-box/ImageButton.tsx
@@ -1,6 +1,9 @@
-import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import { MouseEventHandler } from "react";
+import { useFormStatus } from "react-dom";
+
+import Spinner from "../common/Spinner";
 
 const ImageButton: React.FC<{
   src: string | StaticImport;
@@ -9,10 +12,12 @@ const ImageButton: React.FC<{
   onClick?: MouseEventHandler<HTMLButtonElement>;
   className?: string;
 }> = ({ src, alt, className, type, onClick }) => {
+  const { pending } = useFormStatus();
   const style: string = `w-12 h-12 flex justify-center items-center ${className}`;
+
   return (
     <button type={type} className={style} onClick={onClick}>
-      <Image src={src} alt={alt} />
+      {pending ? <Spinner /> : <Image src={src} alt={alt} />}
     </button>
   );
 };
