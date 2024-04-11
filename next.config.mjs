7f8b43755945643640bb11002cda09e5870c9174
michalrudy9 <michallrudy9@gmail.com>
2024-04-11 22:16:06 +0200

    Added hostname in next config for image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false };
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/logos/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
