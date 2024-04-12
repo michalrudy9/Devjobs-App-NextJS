d2e788086728c2edee53258809498993c3960072
michalrudy9 <michallrudy9@gmail.com>
2024-04-12 09:03:18 +0200

    Removed unnecessary next config entry

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/logos/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
