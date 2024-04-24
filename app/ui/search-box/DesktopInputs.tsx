856866557305dd7cf859db5c3f7fd9ed47247ef9
michalrudy9 <michallrudy9@gmail.com>
2024-04-24 20:48:30 +0200

    Updated desktop inputs component

--- app/ui/search-box/DesktopInputs.tsx
+++ app/ui/search-box/DesktopInputs.tsx
@@ -1,10 +1,16 @@
+import React from "react";
+
 import InputText from "@/app/ui/common/InputText";
 import iconSearch from "@/public/desktop/icon-search.svg";
 import iconLocation from "@/public/desktop/icon-location.svg";
 import InputCheckbox from "@/app/ui/common/inputCheckbox/InputCheckbox";
 import PrimaryButton from "@/app/ui/common/buttons/PrimaryButton";
 
-const DesktopInput = () => {
+const DesktopInput: React.FC<{
+  animatedSubmmit?: boolean;
+  animatedSearchText?: boolean;
+  animatedLocation?: boolean;
+}> = ({ animatedSubmmit, animatedSearchText, animatedLocation }) => {
   return (
     <>
       <InputText
@@ -13,6 +19,9 @@ const DesktopInput = () => {
         name="searchText"
         placeholder="Filter by title, companies, expertise..."
         className="flex w-[30%]"
+        animatedText="Senior Frontend Developer"
+        delay={1.5}
+        animated={animatedSearchText}
       />
       <InputText
         src={iconLocation}
@@ -20,13 +29,20 @@ const DesktopInput = () => {
         name="location"
         placeholder="Filter by location..."
         className="flex w-[30%]"
+        animatedText=" Florida"
+        delay={0.5}
+        animated={animatedLocation}
       />
       <InputCheckbox
         name="fullTime"
         labelText="Full Time"
         className="flex min-w-[8rem]"
       />
-      <PrimaryButton text="Search" className="flex" />
+      <PrimaryButton
+        text="Search"
+        className="flex"
+        animated={animatedSubmmit}
+      />
     </>
   );
 };
