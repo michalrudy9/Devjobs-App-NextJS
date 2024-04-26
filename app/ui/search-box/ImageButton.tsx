4b945f32b175832f30163b1ee35b640665f3ac8d
michalrudy9 <michallrudy9@gmail.com>
2024-04-26 20:44:25 +0200

    Added animation to image button component

--- app/ui/search-box/ImageButton.tsx
+++ app/ui/search-box/ImageButton.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import { MouseEventHandler } from "react";
 import { useFormStatus } from "react-dom";
+import { motion } from "framer-motion";
 
 import Spinner from "@/app/ui/common/Spinner";
 
@@ -11,14 +12,37 @@ const ImageButton: React.FC<{
   type?: "button" | "submit" | "reset" | undefined;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   className?: string;
-}> = ({ src, alt, className, type, onClick }) => {
+  animated?: boolean;
+}> = ({ src, alt, className, type, onClick, animated }) => {
   const { pending } = useFormStatus();
   const style: string = `w-12 h-12 flex justify-center items-center ${className}`;
 
+  const button = {
+    animate: {
+      rotate: [0, -10, 0, 10, 0],
+      transition: {
+        delay: 3,
+        duration: 0.5,
+        ease: "easeInOut",
+      },
+    },
+    whileHover: { scale: 1.1 },
+    whileTap: { scale: 0.9 },
+  };
+
   return (
-    <button type={type} className={style} onClick={onClick}>
+    <motion.button
+      type={type}
+      className={style}
+      onClick={onClick}
+      variants={button}
+      animate={`${animated && "animate"}`}
+      whileHover={`${animated && "whileHover"}`}
+      whileTap={`${animated && "whileTap"}`}
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}
+    >
       {pending ? <Spinner /> : <Image src={src} alt={alt} />}
-    </button>
+    </motion.button>
   );
 };
 
